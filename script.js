462c1deb5b33f5b1c22306b0e2bbf91ea6a669ac
Rahulhomoeohall <dr.rahulbhardwaj.clinic@gmail.com>
2026-08-05 23:07:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,11 +40,7 @@ console.log("Welcome to Rahul Homoeo Hall Website");
 // ==========================
 // Appointment Form
 // ==========================
-const appointmentForm = document.getElementById("appointmentForm");
-
-if (appointmentForm) {
-
-  appointmentForm.addEventListener("submit", async function (e) {
+appointmentForm.addEventListener("submit", async function (e) {
 
     e.preventDefault();
 
@@ -55,42 +51,5 @@ if (appointmentForm) {
       return;
     }
 
-    const data = {
-      name: document.getElementById("name").value,
-      mobile: mobile,
-      age: document.getElementById("age").value,
-      gender: document.getElementById("gender").value,
-      doctor: document.getElementById("doctor").value,
-      problem: document.getElementById("problem").value,
-      date: document.getElementById("date").value,
-      time: document.getElementById("time").value
-    };
-
-    try {
-
-      const response = await fetch("https://script.google.com/macros/s/AKfycbxw3ROB6GYNcy8pxoCJHJ6jPFysciUyxMF7FQ8eNIfyv0lODs7CfRir67C61XxJKC1i8g/exec", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-      });
-
-      if (!response.ok) {
-        throw new Error("Request Failed");
-      }
-
-      alert("Appointment Booked Successfully");
-
-      window.location.href = "success.html";
-
-    } catch (error) {
-
-      alert("Booking Failed. Please Try Again.");
-      console.error(error);
-
-    }
-
-  });
-
-}
+alert("Appointment Booked Successfully");
+window.location.href = "success.html";
